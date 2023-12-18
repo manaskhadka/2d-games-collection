2890ba2e3ad09dd0b0b29810d8fa6b2ec6de2323
Manas Khadka <mkhadka.cs.stanford.edu>
2023-12-17 16:38:41 -0800

    Added simple surface demo

--- tutorial-content/surfaces.py
+++ tutorial-content/surfaces.py
@@ -13,10 +13,21 @@ pygame.init()
 pygame.display.set_caption('Surfaces')
 screen = pygame.display.set_mode((800, 400))
 clock = pygame.time.Clock()
+test_font = pygame.font.Font("tutorial-content/lib/font/Pixeltype.ttf", 50)
 
-test_surface = pygame.Surface((100, 200))
-test_surface.fill("Red")
+test_surface_rect = pygame.Surface((100, 200))
+test_surface_rect.fill("Red")
 
+sky_surface = pygame.image.load("tutorial-content\lib\graphics\Sky.png")
+ground_surface = pygame.image.load("tutorial-content\lib\graphics\ground.png")
+text_surface = test_font.render("Dolting", False, "Blue")
+
+# Creating text: create an image of the text --> render on display surface
+"""
+1. Create a font (text size and style)
+2. Write text on a surface
+3. Blit the text surface
+"""
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -24,7 +35,11 @@ while True:
             print("user exit")
             exit()
 
-    screen.blit(test_surface, (200, 100)) # blit = block image transfer (put a surface on another)
+    # blit = block image transfer (put a surface on another)
+    # screen.blit(test_surface_rect, (200, 100)) 
+    screen.blit(sky_surface, (0, 0))
+    screen.blit(ground_surface, (0, 300))
+    screen.blit(text_surface, (120, 50))
 
     pygame.display.update()
     clock.tick(60)
